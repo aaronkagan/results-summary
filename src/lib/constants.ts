afd4630be483724c0ee28f78dee9772419ec4988
Aaron Kagan <aaronkagan@gmail.com>
2025-01-25 21:35:48 -0500

    initial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = 'Results Summary Card';
+export const APP_DESCRIPTION = 'An app to showcase my frontend skills';
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
